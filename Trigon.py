a9dee39e6d72dab66963cc66dea7d699e4b698ca
Emmanuel Muturia <55001497+emmanuelmuturia@users.noreply.github.com>
2022-05-24 18:51:14 +0300

    Add files via upload
    This commit involves the basic setup of output prompts in the script.

---
+++ Trigon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import math
+
+print("Hello, what's your name?")
+
+name = input()
+
+print("Hello " + name + "! Nice to meet you. Ready for some Trigonometry?")
+
+response = input()
+
+print("Alright! Which trigonometric function do you want to invoke today?")
+print("1. Sin\n2. Cos\n3. Tan")
