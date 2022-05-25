93a66523d26121051336d8eb291c9cde2fffbf78
Emmanuel Muturia <55001497+emmanuelmuturia@users.noreply.github.com>
2022-05-25 19:40:14 +0300

    Update Trigon.py
    This is the final implementation of the three trigonometric functionalities.

--- Trigon.py
+++ Trigon.py
@@ -12,3 +12,23 @@ response = input()
 
 print("Alright! Which trigonometric function do you want to invoke today?")
 print("1. Sin\n2. Cos\n3. Tan")
+
+choice = int(input())
+
+if choice == 1:
+    print("Please enter your number and hit ENTER...")
+    sineNumber = float(input())
+    sineResult = math.sin(sineNumber)
+    print("The sine of your number is " + str(sineResult))
+
+elif choice == 2:
+    print("Please enter your number and hit ENTER...")
+    cosineNumber = float(input())
+    cosineResult = math.cos(cosineNumber)
+    print("The cosine of your number is " + str(cosineResult))
+
+elif choice == 3:
+    print("Please enter your number and hit ENTER...")
+    tanNumber = float(input())
+    tanResult = math.tan(tanNumber)
+    print("The tan of your number is " + str(tanResult))
